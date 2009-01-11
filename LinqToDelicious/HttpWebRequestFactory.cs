420432f0136ca357ae733375e2c2c0d18477d7a0
Matthew Maravillas <maravillas@gmail.com>
2009-01-11 15:53:12 -0500

    Add HttpWebRequestFactory as a wrapper to WebRequest#Create()

---
+++ LinqToDelicious/HttpWebRequestFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace LinqToDelicious
+{
+    class HttpWebRequestFactory : IHttpWebRequestFactory
+    {
+        public HttpWebRequest CreateRequest(string uri)
+        {
+            return (HttpWebRequest)WebRequest.Create(uri);
+        }
+    }
+}
---
+++ LinqToDelicious/IHttpWebRequestFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace LinqToDelicious
+{
+    interface IHttpWebRequestFactory
+    {
+        HttpWebRequest CreateRequest(string uri);
+    }
+}
