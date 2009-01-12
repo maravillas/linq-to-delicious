d8b674da8acaed3ed48c1c7525b43b18c1272924
Matthew Maravillas <maravillas@gmail.com>
2009-01-11 19:28:55 -0500

    Add RequestException

---
+++ LinqToDelicious/RequestException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToDelicious
+{
+    class RequestException : ApplicationException
+    {
+        public RequestException() { }
+        public RequestException(string message) : base(message) { }
+        public RequestException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
