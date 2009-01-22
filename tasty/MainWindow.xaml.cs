601ea0ba38aadf296f72f1ff0ccad8fa5f4f048e
Matthew Maravillas <maravillas@gmail.com>
2009-01-21 19:29:26 -0500

    Add ListBox to window with some default data

--- tasty/MainWindow.xaml.cs
+++ tasty/MainWindow.xaml.cs
@@ -27,18 +27,38 @@ namespace tasty
         public MainWindow()
         {
             InitializeComponent();
-            
-            Posts = new List<Post>();
 
+            Posts = new List<Post>();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
             /*
             var query = from post in new Delicious("username", "password").Posts
-                        where post.Date == new DateTime(2009, 1, 16)
-                        select post;
-
-            foreach (var p in query)
+                        where post.Date == new DateTime(2009, 1, 12)
+                        select post;*/
+            var posts = new List<Post>()
             {
-                Debug.WriteLine(p);
-            }*/
+                new Post(
+                    "http://example.com/",
+                    "a5a6f3d28d8dd549f3cad39fb0b34104",
+                    "Example domain",
+                    "example domain",
+                    "An example site.",
+                    "2008-12-12T07:45:52Z",
+                    "762ee1d713648596931f798a7ba987e0"),
+                new Post(
+                    "http://second-example.com/",
+                    "ce67c6fbe4f79a521481060e2447001b",
+                    "Another example domain",
+                    "example domain another",
+                    "Another example site.",
+                    "2008-12-12T04:04:24Z",
+                    "fa2a46d239ad4f089c3ce7961d958b2e")
+            };
+
+            postListBox.ItemsSource = posts;
         }
 
         private void Exit_Executed(object sender, ExecutedRoutedEventArgs e)
